c94081c7f4bef7ddda83ed5f224b2b08fbc944c0
Johnny <johnnybrown0g@gmail.com>
2026-07-02 23:15:18 -0500

    completed problem 19: vector5
    Was getting an error message during runtime for "var1/var2 contain aggregate value files", which I think had to do with me declaring the wire and assining it on the same line? It fixed itself when I put them directly onto one line. Another note: Double negatives got me here, had to debug real quick.

--- ProblemSets/2.VerilogLanguage/2.Vectors/19.vector5.v
+++ ProblemSets/2.VerilogLanguage/2.Vectors/19.vector5.v
@@ -1,12 +1,9 @@
 module top_module (
     input a, b, c, d, e,
     output [24:0] out );//
-	
-    wire vec1[24:0] = {{5{~a}}, {5{~b}}, {5{~c}}, {5{~d}}, {5{~e}}};
-    wire vec2[24:0] = {5{a,b,c,d,e}};
-    assign out[24:0] = ~vec1 ^ vec2;
+
     // The output is XNOR of two vectors created by 
     // concatenating and replicating the five inputs.
-    // assign out = ~{ ... } ^ { ... };
+    assign out = ~{{5{a}}, {5{b}}, {5{c}}, {5{d}}, {5{e}}} ^ {5{a,b,c,d,e}};
 
 endmodule
